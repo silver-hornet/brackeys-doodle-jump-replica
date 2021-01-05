53e13e7ab7ea86a3d2e5901c1a3cd233d747255a
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-05 09:23:48 -0500

    Completed tutorial

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void LateUpdate()
+    {
+        if (target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float movementSpeed = 10f;
+    float movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
