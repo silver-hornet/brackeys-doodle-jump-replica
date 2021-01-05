e14e04181d6891cf72682be41f22b3e4a4278310
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-04 20:52:15 -0500

    Initial commit

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 10f;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D rb = other.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
+        }
+    }
+}
